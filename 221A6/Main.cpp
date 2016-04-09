1045498395a95726d285dec740a98c06fca2788c
Matt G <matt.gaikema@tamu.edu>
2016-04-09 12:12:33 -0500

    command line arguments hard

--- 221A6/Main.cpp
+++ 221A6/Main.cpp
@@ -3,11 +3,23 @@
 */
 
 #include <iostream>
+#include <fstream>
 #include "Graph.h"
 
 using namespace std;
 
-int main(int argc, const char * argv[]){
+int main(int argc, const char * argv[]) {
+	
+	ifstream infile;
+	
 	cout << "Hello, world." << endl;
+	cout << *argv[1] << endl;
+
+	infile.open(argv[1]);
+
+	if (!infile) {
+		cout << "Error!" << endl;
+	}
+
 	return 0;
 }
