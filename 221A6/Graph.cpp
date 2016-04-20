daeddaf6c920c7856ca9ee20b7149ea19df5101d
Matt G <matt.gaikema@tamu.edu>
2016-04-20 15:57:50 -0500

    works for input 1

--- 221A6/Graph.cpp
+++ 221A6/Graph.cpp
@@ -220,14 +220,16 @@ Graph Graph::get_acyclic() {
 	}
 
 	for (auto& v : acycle.vertices) {
-		for (auto& e : v.edge_list) {
+		for (auto e = v.edge_list.begin(); e!=v.edge_list.end();) {
 			// If e.end is not in acyclic_vertices,
 			// replace it with magic.
 			
-			if (!contains(acyclic_vertices, e.end)) {
+			if (!contains(acyclic_vertices, e->end) && e->end!=magic.label) {
 				//e.end = magic.label;
-				//e = v.edge_list.erase(e);
+				e = v.edge_list.erase(e);
 			}
+			else
+				e++;
 
 		}
 	}
