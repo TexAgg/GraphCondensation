b2033a7aaca81164e1d18c4bcfeff5717c925ff1
Matt G <matt.gaikema@tamu.edu>
2016-04-20 15:44:30 -0500

    gonna use a set

--- 221A6/Graph.cpp
+++ 221A6/Graph.cpp
@@ -219,14 +219,16 @@ Graph Graph::get_acyclic() {
 		acycle.insert(k, magic.label);
 	}
 
-	for (auto& v : vertices) {
+	for (auto& v : acycle.vertices) {
 		for (auto& e : v.edge_list) {
 			// If e.end is not in acyclic_vertices,
 			// replace it with magic.
-
-			if (!contains(acyclic_vertices, e.end) && e.start!=magic.label) {
-				e.end = magic.label;
+			
+			if (!contains(acyclic_vertices, e.end)) {
+				//e.end = magic.label;
+				//e = v.edge_list.erase(e);
 			}
+
 		}
 	}
 
