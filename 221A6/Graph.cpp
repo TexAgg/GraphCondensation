7c32bb329f26d29ef7681e57c8450e6c9ee6c485
Matt G <matt.gaikema@tamu.edu>
2016-04-21 23:50:01 -0500

    paste link

--- 221A6/Graph.cpp
+++ 221A6/Graph.cpp
@@ -227,6 +227,7 @@ Graph Graph::get_acyclic() {
 
 	// TODO: Remove extra vertices.
 
+	// https://bitbucket.org/gaikema/csce221-a6/src/45cf9e882d1043d50426be136f5fdf9950509077/221A6/Graph.cpp?at=acyclic&fileviewer=file-view-default
 	for (auto& v : acycle.vertices) {
 		for (auto& e : v.edge_list) {
 			// If e.end \not\in acycle.vertices,
